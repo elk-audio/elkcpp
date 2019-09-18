3f2f3b630b8cd8a6d1ec565fc4644484d8a7a46b
rubs-mind <therubething@gmail.com>
2019-09-18 06:09:34 -0400

    Implemented simple helper types

--- include/sushi_info_types.h
+++ include/sushi_info_types.h
@@ -7,6 +7,9 @@
 #ifndef SUSHI_INFO_TYPES_H
 #define SUSHI_INFO_TYPES_H
 
+namespace sushi_controller
+{
+    
 /**
  * @brief Enum mapping parameter type to ints.
  */
@@ -44,10 +47,62 @@ enum class SYNC_MODE
  */
 struct TimeSignature
 {
-    TimeSignature(int numerator, int denominator) : numerator(numerator), denominator(denominator) {};
-
     int numerator;
     int denominator;
+};
+
+/**
+ * @brief Structure to hold timings
+ */
+struct CpuTimings
+{
+    float average;
+    float min;
+    float max;
+};
+
+/**
+ * @brief Object to hold info about a track
+ */
+struct TrackInfo
+{
+    int id;
+    std::string label;
+    std::string name;
+    int input_channels;
+    int input_busses;
+    int output_channels;
+    int output_busses;
+    int processor_count;
+};
+
+/**
+ * @brief Object to hold info about a processor
+ */
+struct ProcessorInfo
+{
+    int id;
+    std::string label;
+    std::string name;
+    int parameter_count;
+    int program_count;
+};
+
+/**
+ * @brief Object to hold info about a parameter
+ */
+struct ParameterInfo
+{
+    int id;
+    std::string type;
+    std::string label;
+    std::string name;
+    std::string unit;
+    bool automatable;
+    float min_range;
+    float max_range;
+};
+
 }
 
 #endif // SUSHI_INFO_TYPES_H
