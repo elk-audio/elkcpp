cc039b4d0d5a71ed46585446be41a4deb94e1967
rubs-mind <therubething@gmail.com>
2019-10-03 08:29:41 -0400

    Updated the cmake structure

--- test/unittests/sushi_client_integration_test.cpp
+++ test/unittests/sushi_client_integration_test.cpp
@@ -3,9 +3,8 @@
 
 #include "gtest/gtest.h"
 
-
-//#include "sushi_grpc_client.h"
 #include "sushi_client.h"
+#include "sushi_grpc_client.cpp"
 #include "test_utils/sushi_server_mockup.h"
 #define private public
 
