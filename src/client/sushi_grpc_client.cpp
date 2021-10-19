a2eab1529df72411b51bdf42770f916ae6dad5e5
Ruben Svensson <ruben@elk.audio>
2021-10-19 17:35:10 +0200

    Uncomment controllers

--- src/client/sushi_grpc_client.cpp
+++ src/client/sushi_grpc_client.cpp
@@ -7,30 +7,30 @@ namespace sushi_controller
 
 SushiControllerClient::SushiControllerClient(const std::string& address)
     : SushiController(
-        nullptr, // &_system_controller_client,
+        &_system_controller_client,
         &_transport_controller_client,
         &_timing_controller_client,
         &_keyboard_controller_client,
         &_audio_graph_controller_client,
         &_program_controller_client,
         &_parameter_controller_client,
-        nullptr, // &_midi_controller_client,
-        nullptr, // &_audio_routing_controller_client,
-        nullptr, // &_cv_gate_controller_client,
-        nullptr, // &_osc_controller_client,
+        &_midi_controller_client,
+        &_audio_routing_controller_client,
+        &_cv_gate_controller_client,
+        &_osc_controller_client,
         &_notification_controller_client
     ),
-    // _system_controller_client(address),
+    _system_controller_client(address),
     _transport_controller_client(address),
     _timing_controller_client(address),
     _keyboard_controller_client(address),
     _audio_graph_controller_client(address),
     _program_controller_client(address),
     _parameter_controller_client(address),
-    // _midi_controller_client(address),
-    // _audio_routing_controller_client(address),
-    // _cv_gate_controller_client(address),
-    // _osc_controller_client(address),
+    _midi_controller_client(address),
+    _audio_routing_controller_client(address),
+    _cv_gate_controller_client(address),
+    _osc_controller_client(address),
     _notification_controller_client(address) {}
 
 std::shared_ptr<SushiController> CreateSushiController(const std::string& server_address)
--- src/client/sushi_grpc_client.h
+++ src/client/sushi_grpc_client.h
@@ -14,17 +14,17 @@
 #include "sushi_rpc.grpc.pb.h"
 
 #include "control_interface.h"
-// #include "system_controller.h"
+#include "system_controller.h"
 #include "transport_controller.h"
 #include "timing_controller.h"
 #include "keyboard_controller.h"
 #include "audio_graph_controller.h"
 #include "program_controller.h"
 #include "parameter_controller.h"
-// #include "midi_controller.h"
-// #include "audio_routing_controller.h"
-// #include "cv_gate_controller.h"
-// #include "osc_controller.h"
+#include "midi_controller.h"
+#include "audio_routing_controller.h"
+#include "cv_gate_controller.h"
+#include "osc_controller.h"
 #include "notification_controller.h"
 
 namespace sushi_controller
@@ -41,17 +41,17 @@ public:
     SushiControllerClient(const std::string& address);
 
 private:
-    // SystemControllerClient _system_controller_client;
+    SystemControllerClient _system_controller_client;
     TransportControllerClient _transport_controller_client;
     TimingControllerClient _timing_controller_client;
     KeyboardControllerClient _keyboard_controller_client;
     AudioGraphControllerClient _audio_graph_controller_client;
     ProgramControllerClient _program_controller_client;
     ParameterControllerClient _parameter_controller_client;
-    // MidiControllerClient _midi_controller_client;
-    // AudioRoutingControllerClient _audio_routing_controller_client;
-    // CvGateControllerClient _cv_gate_controller_client;
-    // OscControllerClient _osc_controller_client;
+    MidiControllerClient _midi_controller_client;
+    AudioRoutingControllerClient _audio_routing_controller_client;
+    CvGateControllerClient _cv_gate_controller_client;
+    OscControllerClient _osc_controller_client;
     NotificationControllerClient _notification_controller_client;
 };
 
